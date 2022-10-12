3ffe32e16fd648575b993468575aebd933377afe
Nikss <nik.safe@gmail.com>
2022-10-12 16:11:59 -0700

    add hello function

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
-
+import App, {Hello} from './App';
+console.log(App,"<>>>APP")
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('hello',() => {
+  expect(Hello()).toBe('hello')
+})
